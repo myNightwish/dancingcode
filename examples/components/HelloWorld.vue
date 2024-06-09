bc10588b08a4706cc40dc5bc1a3522a6df0f21f5
zhoumaoling <zhoumaoling@baidu.com>
2024-06-10 00:38:29 +0800

    ✨组件化-基础-icon

--- examples/components/HelloWorld.vue
+++ examples/components/HelloWorld.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <h1>welcome to My Vue.js UI By WebPack!</h1>
         <me-loading loading-text="页面加载中...."></me-loading>
-        <mi-icon name="star-off"></mi-icon>
+        <mi-icon name="loading"></mi-icon>
     </div>
 </template>
 
